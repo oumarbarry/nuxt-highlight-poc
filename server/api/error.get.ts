55a853714c18db18fe94d9394c1e3a71694cebd1
Oumar Barry <oumar.barry1202@gmail.com>
2023-03-25 02:40:57 +0000

    chore: trying highlight.io log ingestion

--- server/api/error.get.ts
+++ server/api/error.get.ts
@@ -4,6 +4,9 @@ export default defineEventHandler((event) => {
   const headers = getRequestHeaders(event)
   const parsed = H.parseHeaders(headers)
 
+  console.log('hey there!');
+  console.warn('whoa there', {'key': 'value'});
+
   try {
     throw new Error('example error!')
   } catch (e: any) {
